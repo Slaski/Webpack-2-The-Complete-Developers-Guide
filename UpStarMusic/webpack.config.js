dcf591b2432eeac21c9b23a708064ba40ed87f0e
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-06 17:47:38 -0300

    feat: Add cache busting to webpack.

--- UpStarMusic/webpack.config.js
+++ UpStarMusic/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -37,7 +37,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+      name: ['vendor', 'manifest']
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
