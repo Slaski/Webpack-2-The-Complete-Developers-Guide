0cff2f48648ee1b70f3298561d2da43d28917cf3
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-06 11:46:03 -0300

    feat: Add babel to transpile javascript files.

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -6,7 +6,15 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  }
 };
 
 module.exports = config;
