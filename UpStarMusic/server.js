739c8167c73ee383cfa39183f51ea6e0eb0171c6
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-09 10:55:48 -0300

    fix: Set the express app to use a specified PORT variable.

--- UpStarMusic/server.js
+++ UpStarMusic/server.js
@@ -17,4 +17,6 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-app.listen(3050, () => console.log('Listening on port 3050.'));
+app.listen(process.env.PORT || 3050, () =>
+  console.log('Listening on port 3050.')
+);
